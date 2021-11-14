70f61ef96a0e0e0a4f68a2a4f6af7ba1618c66ff
Renan Perez <perez.vr@gmail.com>
2021-11-13 22:45:55 -0300

    refactoring

---
+++ HandTelegram.Bot.Infra.Integrations/Abstractions/GetWeather/ForecastDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandTelegram.Bot.Infra.Integrations.Abstractions.GetWeather
+{
+    public class ForecastDto
+    {
+        public string date { get; set; }
+        public string weekday { get; set; }
+        public int max { get; set; }
+        public int min { get; set; }
+        public string description { get; set; }
+        public string condition { get; set; }
+    }
+}
---
+++ HandTelegram.Bot.Infra.Integrations/Abstractions/GetWeather/GetWeatherResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandTelegram.Bot.Infra.Integrations.Abstractions.GetWeather
+{
+    public class GetWeatherResponseDto
+    {
+        public int temp { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public string condition_code { get; set; }
+        public string description { get; set; }
+        public string currently { get; set; }
+        public string cid { get; set; }
+        public string city { get; set; }
+        public string img_id { get; set; }
+        public int humidity { get; set; }
+        public string wind_speedy { get; set; }
+        public string sunrise { get; set; }
+        public string sunset { get; set; }
+        public string condition_slug { get; set; }
+        public string city_name { get; set; }
+        public List<ForecastDto> forecast { get; set; }
+    }
+}
